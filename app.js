1db6696843fab930dd6efac5b958c8fc185915dd
Quan Huynh <qlhuynh@gmail.com>
2020-02-17 18:15:39 -0800

    Initial file setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const prompt = require('inquirer').createPromptModule()
+let teamMembersArr = []
+let teamLeader = ''
+let teamLeaderAnswer = ''
+
+
+let teamNumQues = [
+  'Who is the team leader?',
+  'How many team members?'
+]
+
+let questions = [
+  '',
+  'What is your favorite movie?',
+  'When is your birthday?',
+  'What is your favorite book?',
+  ''
+]
