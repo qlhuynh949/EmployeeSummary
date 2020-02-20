dd71b0ddef8a2ca8de633f7b7e61cc2198894a29
Quan Huynh <qlhuynh@gmail.com>
2020-02-19 23:18:53 -0800

    added preliminary html templates and questions

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const Engineer = require('Engineer')
 const Intern = require('Intern')
 const Manager = require('Manager')
 let teamMembersArr = []
-let teamLeader = ''
-let teamLeaderAnswer = ''
 
 
 let teamNumQues = [
@@ -14,10 +12,10 @@ let teamNumQues = [
 
 let managerQuestions = [
   'Please enter the name of the manager?',
- 'What is the office number?'
+  'What is the office number?'
 ]
 
-let engineerQuestions= [
+let engineerQuestions = [
   'What is your gitHub? '
 ]
 
@@ -29,5 +27,118 @@ let questions = [
   'What is your name?',
   'What is id?',
   'What is your email?'
-  'What is role?',
+  'What is your role?',
 ]
+
+async function startQuestions() {
+  const initPrompt = await prompt([{
+    type: 'input',
+    name: 'teamNumber',
+    message: teamNumQues[0]
+  },
+  {
+    type: 'input',
+    name: 'managerName',
+    message: managerQuestions[0]
+  },
+  {
+    type: 'input',
+    name: 'managerOffice',
+    message: managerQuestions[1]
+  },
+  {
+    type: 'input',
+    name: 'managerId',
+    message: questions[1]
+  },
+  {
+    type: 'input',
+    name: 'managerEmail',
+    message: questions[2]
+  }
+  ])
+    .then({ teamNumber, managerName, managerOffice, managerId, managerEmail } => {
+    teamNumber = response.teamNumber
+    let lead = new Manager(managerName, managerId, managerEmail, managerOffice)
+    teamMembersArr.push(lead)
+    askTeamMembers(teamNumber - 1)
+
+
+  })
+    .catch (e => console.error(e))
+}
+
+async function askTeamMembers(teamNumber) {
+  for (let i = 0; i < teamNumber; i++) {
+    const internEngineer = await prompt([{
+      type: 'input',
+      name: `memberName`,
+      message: questions[0]
+    }, {
+      type: 'input',
+      name: `memberRole`,
+      message: questions[3]
+    }])
+      .then(({ memberName, memberRole }) => {
+        if (memberRole === 'Engineer') {
+          const engineerPrompt = await prompt([{
+            type: 'input',
+            name: `memberId`,
+            message: questions[1]
+          }, {
+            type: 'input',
+            name: `memberEmail`,
+            message: questions[2]
+          },
+          {
+            type: 'input',
+            name: `memberGithub`,
+            message: engineerQuestions[0]
+          }
+          ]).then(({ memberId, memberEmail, memberGithub }) => {
+
+            let engineerMember = new Engineer(memberName, memberId, memberEmail, memberGithub)
+
+            teamMembersArr.push(engineerMember)
+
+          }
+          )
+        }
+        else {
+          const internPrompt = await prompt([{
+            type: 'input',
+            name: `memberId`,
+            message: questions[1]
+          }, {
+            type: 'input',
+            name: `memberEmail`,
+            message: questions[2]
+          },
+          {
+            type: 'input',
+            name: `memberSchool`,
+            message: internQuestions[0]
+          }
+          ]).then(({ memberId, memberEmail, memberSchool }) => {
+
+            let internMember = new Engineer(memberName, memberId, memberEmail, memberSchool)
+
+            teamMembersArr.push(internMember)
+
+
+          }
+          )
+        }
+
+      })
+  }
+
+  // display each team member
+  teamMembersArr.forEach((elem, i) => {
+    console.log(elem)
+
+  })
+
+  startQuestions()
+
+}
